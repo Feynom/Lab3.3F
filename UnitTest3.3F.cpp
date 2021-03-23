0904c5d9edd505819f4b3836e29cc5b24f3ec283
Feynom <53947611+Feynom@users.noreply.github.com>
2021-03-23 19:43:03 +0200

    Add files via upload

---
+++ UnitTest3.3F.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab3.3F/Lab3.3F/Angle.h"
+#include "../Lab3.3F/Lab3.3F/Angle.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest33F
+{
+	TEST_CLASS(UnitTest33F)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Angle A(90, 0), B(180, 0);
+			Assert::AreEqual(round(A.getSin()), 1.);
+			Assert::AreEqual(round(B.getSin()), 0.);
+		}
+	};
+}
